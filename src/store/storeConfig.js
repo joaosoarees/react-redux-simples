276fee2d067d4800fc620c036b2e3ab37bd3b104
João Vitor <jsoareez@gmail.com>
2020-10-29 21:51:01 -0300

    create first store

---
+++ src/store/storeConfig.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers } from 'redux';
+
+const reducers = combineReducers({
+  numbers: function(state, action) {
+    console.log(state, ' ', action)
+    return {
+      min: 1,
+      max: 10
+    }
+  },
+  names: function(state, action) {
+    console.log(state, ' ', action)
+    return [
+      'João',
+      'Bia',
+      'Carlos'
+    ]
+  }
+});
+
+function storeConfig() {
+  return createStore(reducers)
+};
+
+export default storeConfig;
